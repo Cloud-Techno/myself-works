c6d80b8f5da24e3fd4125b1005dcb38fa300f82a
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-10 12:23:25 +0300

    this structures

--- this/app.js
+++ this/app.js
@@ -12,8 +12,7 @@
 
 
 //class version
-
-/* class Person {
+class Person {
     constructor(name,surname,age){
         this.name = name;
         this.surname=surname;
@@ -29,17 +28,25 @@ const ferdi = new Person ("Ferdi","Ates",32);
 const derya = new Person ("Derya","Ates",32);
 
 console.log(ferdi);
-console.log(derya); */
+console.log(derya); 
 
+
+//////////////////////////////
+/* 
 const ferdi = {
     name: "Ferdi",
     age: 5,
     surname: "ates",
     fullName: function(){
-        console.log(this);
+        console.log(this); // reference in this object: output "ferdi" 5 "ates"
         return this.name + " " + this.surname; 
     }
 }
 
 console.log(ferdi);
-console.log(ferdi.fullName());
+console.log(ferdi.fullName()); */
+
+//output :
+//{name: "Ferdi", age: 5, surname: "ates", fullName: ƒ}
+//{name: "Ferdi", age: 5, surname: "ates", fullName: ƒ}
+//Ferdi ates
