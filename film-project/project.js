a51535bc5cae5df12d5c6e40df6f37c3cc0846a8
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-11 11:12:04 +0300

    film project

---
+++ film-project/project.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("todo-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url")
