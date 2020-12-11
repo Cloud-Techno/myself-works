3b611ccb4b007f4f0f2de52d0ce1c4e9211f25f3
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-11 11:52:41 +0300

    film

--- film-project/project.js
+++ film-project/project.js
@@ -1,4 +1,36 @@
-const form = document.getElementById("todo-form");
+const form = document.getElementById("film-form");
 const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector("#director");
-const urlElement = document.querySelector("#url")
+const urlElement = document.querySelector("#url");
+
+// Start UI Object
+
+const ui = new UI();
+
+//All events upload
+
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener("submit",addFilm);
+
+}
+
+function addFilm(e){
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+if ( title === "" || director === "" || url === ""){
+    //Error warning
+}
+    else {
+        /* newFilm */
+        const newFilm = new Film(title,director,url);
+
+        ui.addFilmToUI(newFilm);
+    }
+
+
+    e.preventDefault();
+}
