f952832936cdefb59d2d55a39888a1cdb8ddfded
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-19 11:18:42 +0300

    add book to UI

--- book-project/project.js
+++ book-project/project.js
@@ -1,43 +1,36 @@
 const form = document.getElementById('book-form');
-const titleElement = document.getElementById('title');
+const titleElement = document.getElementById ('title');
 const autorElement = document.getElementById('autor');
 const urlElement = document.getElementById('url');
 
-
 const ui = new UI();
-const storage = new Storage();
 
 eventListener();
 
- function eventListener(){
+function eventListener(){
     form.addEventListener('submit',addBook);
-    document.addEventListener('DOMContentLoaded', function(){
-        const books = storage.getBookFromStorage();
+}
 
-        ui.loadAllBooks(books);
+function addBook(e){
 
-    })
+    const title= titleElement.value();
+    const autor= autorElement.value();
+    const url= urlElement.value();
 
- }  
+    if (title ==='' || autor === "" || url === ''){
+        //displaye messages
+    }
+    else {
 
- function addBook(e){
-     const title = titleElement.value;
-     const autor = autorElement.value;
-     const url = urlElement.value;
-    
-
-     if (title === '' || autor === "" || url === ""){
-        ui.displayMessages('Please enter all info....','danger');
-     }
-     else{
-         const newBook = new Book(title,autor,url);
+        const newBook = new Book(title,autor,url);
 
         ui.addBookToUI(newBook);
-         storage.addBookToStorage(newBook);
-         ui.displayMessages('Successful....','success');
-     }
-     ui.clearInputs(titleElement,autorElement,urlElement);
-     e.preventDefault();
- }
- 
- console.log('%cproject.js line:43 object', 'color: #007acc;', object);
+
+    }
+
+
+
+
+    e.preventDefault();
+}
+
--- book-project/ui.js
+++ book-project/ui.js
@@ -1,47 +1,4 @@
-function UI (){
+function UI(){
 
 }
 
-UI.prototype.addBookToUI = function(newBook){
-    const booksList = document.getElementById('books');
-
-    booksList.innerHTML +=`
-            <tr>
-            <td><img src="${newBook.url}" class="img-fluid img-thumbnail"></td>
-            <td>${newBook.title}</td>
-            <td>${newBook.autor}</td>
-            <td><a href="#" id = "delete-book" class = "btn btn-danger">Filmi Sil</a></td>
-            </tr>
-      `;
-}
-UI.prototype.loadAllBooks = function(books){
-    const bookList = document.getElementById('books');
-
-    books.forEach(function(book){
-        bookList.innerHTML +=`
-        <tr>
-            <td><img src="${book.url}" class="img-fluid img-thumbnail"></td>
-            <td>${book.title}</td>
-            <td>${book.autor}</td>
-            <td><a href="#" id = "delete-book" class = "btn btn-danger">Filmi Sil</a></td>
-            </tr>`;
-    });
-}
-
-UI.prototype.displayMessages= function(message,type){
-    const cardBody = document.querySelector('.card-body');
-
-
-    const div = document.createElement('div');
-    div.className = `alert alert-${type}`;
-    div.textContent = message;
-    cardBody.appendChild(div);
-}
-
-UI.prototype.clearInputs = function (element1,element2,element3){
-    element1.value = '';
-    element2.value = '';
-    element3.value = '';
-}
-
-console.log(this);
