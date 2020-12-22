21f7a69a025d33b53ba7f369f83a748c0cb242ae
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-22 08:15:38 +0300

    add Film to UI

--- film-project/project.js
+++ film-project/project.js
@@ -12,8 +12,17 @@ function eventListener(){
 }
 
 function addFilm(e){
-    
+     const title = titleElement.value;
+     const director = directorElement.value;
+     const url = urlElement.value;
 
+     if(title === '' || director === "" || url===""){ // check to input value fill or empty????
+         //Message display
+     }
+     else {
+         const newBook = new newBook(title,director,url);
+         ui.addBookToUI(newBook);
+     }
 
-    e.preventDefault();
+     e.preventDefault();
 }
