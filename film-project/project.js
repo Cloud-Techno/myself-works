6a928522a9e61114513e263483228af5e157ebac
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-24 08:53:52 +0300

    click delete film

--- film-project/project.js
+++ film-project/project.js
@@ -2,6 +2,7 @@ const form = document.getElementById('film-form');
 const titleElement = document.getElementById('title');
 const directorElement = document.getElementById('director');
 const urlElement = document.getElementById('url');
+const cardBody = document.querySelectorAll('.card-body')[1];
 
 const ui = new UI();
 const storage = new Storage();
@@ -10,13 +11,20 @@ eventListener();
 
 function eventListener(){
     form.addEventListener('submit',addFilm);
+    cardBody.addEventListener('click',deleteFilm);
     document.addEventListener('DOMContentLoaded',function(){
         let films = storage.getFilmFromStorage();
         ui.loadAllFilms(films);
 
-    })
+    });
 }
+function deleteFilm(e){
+    if (e.target.id ==='delete-film')
 
+    e.target.parentElement.parentElement.remove();
+    ui.displayMessages('film is deleted','success');
+
+}
 function addFilm(e){
     const title = titleElement.value;
     const director = directorElement.value;
