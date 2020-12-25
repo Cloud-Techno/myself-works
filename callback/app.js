6dfc7565923fce665cac789f3edbc16c30760471
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-25 10:20:47 +0300

    callback studies

--- callback/app.js
+++ callback/app.js
@@ -1,4 +1,4 @@
-function process1(callback){
+/*  function process1(callback){
     setTimeout(function(){
         console.log('Process 1');
         callback();
@@ -12,4 +12,26 @@ function process2(){
     },2000);
 }
 
-process1(process2);
+process1(process2); */ 
+
+/////////////////////
+
+const langs = ['Python','Java','C++'];
+
+function addLang(lang,callback){
+    setTimeout(function(){
+        langs.push(lang);
+        console.log('Added');
+        callback();
+    },2000);
+}
+function getAllLangs(){
+    setTimeout(function(){
+        langs.forEach(function(lang){
+            console.log(lang);
+        });
+    },2000);
+}
+
+addLang('Go',getAllLangs);
+
