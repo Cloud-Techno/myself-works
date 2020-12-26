9873acf2e6f498fc00b04926bb8352a1221dafba
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-26 11:01:44 +0300

    post studies

--- ajax-get/app.js
+++ ajax-get/app.js
@@ -5,17 +5,50 @@ class Request{
         this.xhr = new XMLHttpRequest();
     }
     // GET REQUEST
-    get(url){
+    get(url,callback){
         this.xhr.open('GET',url);
         this.xhr.onload= ()=>{
             if(this.xhr.status ===200){
-                console.log(this.xhr.responseText);
+                callback(null,this.xhr.responseText);
+            }
+            else{
+                //hata durumunda
+                callback('ERROR',null);
             }
         }
         this.xhr.send();
+    }
+    post(url,data,callback){
+        this.xhr.open('POST',url);
+        this.xhr.setRequestHeader('Content-type','application/json'); //json adjusted
+
+        this.xhr.onload=()=>{
+            if(this.xhr.status ===201){
+                callback(null,this.xhr.responseText);
+            }
+            else{
+                console.log('there is sth wrong on post',null);
+            }
+        }
+        this.xhr.send(JSON.stringify(data));
     }
     
 }
 
 const request = new Request;
-request.get('https://jsonplaceholder.typicode.com/albums');
+/* request.get('https://jsonplaceholder.typicode.com/albums',function(err,response){
+    if (err==null){
+        console.log(response);
+    }
+    else{
+        console.log(err)
+    }
+}); */
+request.post("https://jsonplaceholder.typicode.com/albums",{userId:2,title:"Thriller"},function(err,album){
+    if (err==null){
+        console.log(album);
+    }
+    else{
+        console.log(err)
+    }
+});
