988e59a81384d140c7fa318aec58fdcdd73fff1a
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-26 15:53:23 +0300

    fetch get function

--- fetch-get/app.js
+++ fetch-get/app.js
@@ -1,21 +1,19 @@
-class Request {
-    get(url){//get Request
-        return new Promise((resolve,reject)=>{
-            fetch(url)
-            .then(response => response.text())
-            .then(data => console.log(data))
-            .catch(err => reject(err))
+class Request{
 
-        })
-        
-    }
+
+  get(url){
+    return new Promise(function(resolve,reject){
+      fetch(url)
+    .then(response =>response.json())
+    .then(data=>resolve(data))
+    .catch(err=>reject(err))
+    })
+      }
 }
 
 const request = new Request();
 let album;
 request.get('https://jsonplaceholder.typicode.com/albums')
-.then(data=>{
-    album = data;
-    console.log(album);
-})
-.catch(err =>console.log(err));
+
+.then(album=>console.log(album))
+.catch(err => console.log(err))
