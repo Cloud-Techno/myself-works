0f365c864681dd46f02294866d86e94b9208079d
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-26 10:41:17 +0300

    get studies

--- ajax-get/app.js
+++ ajax-get/app.js
@@ -4,53 +4,18 @@ class Request{
     constructor(){
         this.xhr = new XMLHttpRequest();
     }
-    //get request
-    get(url,callback){
-       this.xhr.open('GET',url);
-        this.xhr.onload = ()=>{
-        
+    // GET REQUEST
+    get(url){
+        this.xhr.open('GET',url);
+        this.xhr.onload= ()=>{
             if(this.xhr.status ===200){
-                callback(null,this.xhr.responseText);
+                console.log(this.xhr.responseText);
             }
-            else{
-                callback('there is something wrong',null)
-            }
-
         }
-       this.xhr.send();
+        this.xhr.send();
     }
-}
-post(url,data,callback){
-        this.xhr.open('POST',url);
-        this.xhr.setRequestHeader('Content-type','application/json');
-
-        this.xhr.onload = () => {
-            if (this.xhr.status === 201){
-                callback(null,this.xhr.responseText);
-            }
-            else{
-                callback('ERROR',null);
-            }
-            this.xhr.send(JSON.stringify(data));
-        }
-    }
+    
 }
 
-/* const request = new Request();
-request.post('https://jsonplaceholder.typicode.com/albums',{userId:2,title:'Triller'},function(err,response){
-    if(err === null){
-        console.log('success')
-    }
-    else{
-        console.log(err);
-    }
-}); */
-const request = new Request();
-request.post('https://jsonplaceholder.typicode.com/albums/10',{userId:12,title:'tarkan karma'},function(err,response){
-    if(err === null){
-        console.log('success')
-    }
-    else{
-        console.log(err);
-    }
-});
+const request = new Request;
+request.get('https://jsonplaceholder.typicode.com/albums');
